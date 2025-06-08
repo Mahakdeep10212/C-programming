6454fa48464b452bfd64e9135027e56adc76c555
Mahakdeep kaur <Gungun10212@gmail.com>
2025-06-08 15:56:05 +0530

    Add files via upload

---
+++ prog2.cpp
@@ -0,0 +1,11 @@
+//input a no.and print area of circle
+#include<stdio.h>
+int main(){
+float r;
+printf("radius of circle: ");
+scanf("%f",&r);
+float area ;
+area=3.14*r*r;
+printf(" area of circle: %.2f\n",area);
+return 0;	
+}
