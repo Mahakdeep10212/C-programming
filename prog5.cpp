b76110a4b434fd62b0842f5d59863133b4b40290
Mahakdeep kaur <Gungun10212@gmail.com>
2025-06-08 16:35:19 +0530

    Add files via upload

---
+++ prog5.cpp
@@ -0,0 +1,13 @@
+//a program to check even or odd no.
+#include<stdio.h>
+int main(){
+	int a ;
+	printf("enter the value of variable: ");
+	scanf("%d",&a);
+	if(a%2==0){printf("no is even");
+	}
+	else{
+		printf("no.is odd");
+	}
+	return 0;	
+}
